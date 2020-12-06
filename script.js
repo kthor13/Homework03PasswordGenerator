9ff6abaaa47313091c13986ae370ea8397a05a87
K Thor <katie.thorsen7@gmail.com>
2020-12-06 15:15:57 -0500

    Added comments to script and started password generation function.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var upperCase;
 var numeric;
 var specialChar;
 
+// Function to request user's password requirements.
 function pwdRequirements() {
     pwdLength = prompt("How long do you want your password to be? Choose a length between 8 and 128.");
     lowerCase = confirm("Do you want to include lower case letters?");
@@ -12,6 +13,13 @@ function pwdRequirements() {
     numeric = confirm("Do you want to include numbers?");
     specialChar = confirm("Do you want to include special characters?");
 
+}
+
+//Function to generate password.
+function generatePwd() {
+    for (var i = 0; i < pwdLength.length; i++) {
+        ;
+      }
 }
 
 //document.getElementById("btn").addEventListener("click", pwdRequirements());
