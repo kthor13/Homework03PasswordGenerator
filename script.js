484a0f3050c2189beeb0033c0c948489f2160935
K Thor <katie.thorsen7@gmail.com>
2020-12-08 18:11:35 -0500

    Updated BootStrap and jquery links.

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var upperCase;
 var numeric;
 var specialChar;
 
+// Available character arrays
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specials = []
+
 // Function to request user's password requirements.
+document.getElementById("btn").onclick(pwdRequirements());
 function pwdRequirements() {
     pwdLength = prompt("How long do you want your password to be? Choose a length between 8 and 128.");
     lowerCase = confirm("Do you want to include lower case letters?");
