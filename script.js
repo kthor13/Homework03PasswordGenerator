189e85a812be20421fc23dfd478e14a0a28ec422
K Thor <katie.thorsen7@gmail.com>
2020-12-10 18:31:11 -0500

    Trying to get button to work.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Variables
+var buttonEl = document.querySelector("#pwdButton");
 var pwdLength;
 var lowerCase;
 var upperCase;
 var numeric;
 var specialChar;
+console.log("button:", buttonEl);
 
 // Available character arrays
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -11,27 +13,38 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specials = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=']; 
 
 // Function to request user's password requirements.
-document.getElementById("btn").onclick(pwdRequirements());
+//generateBtn.addEventListener('click', writePassword);
+
+
 function pwdRequirements() {
     pwdLength = prompt("How long do you want your password to be? Choose a length between 8 and 128.");
     lowerCase = confirm("Do you want to include lower case letters?");
     upperCase = confirm("Do you want to include upper case letters?");
     numeric = confirm("Do you want to include numbers?");
     specialChar = confirm("Do you want to include special characters?");
+//Build arrays based on parameters
+if (lowerCase === true && upperCase === true && numeric === true && specialChar === true) {
+    
+}
 
 }
+
 
 //Function to generate password.
 function generatePwd() {
     for (var i = 0; i < pwdLength.length; i++) {
-        ;
+        //var password = ;
       }
 }
 
 //document.getElementById("btn").addEventListener("click", pwdRequirements());
-pwdRequirements();
+
+//pwdRequirements();
+
 console.log(pwdLength);
 console.log(lowerCase);
 console.log(upperCase);
 console.log(numeric);
 console.log(specialChar);
+
+buttonEl.addEventListener('click', pwdRequirements);
